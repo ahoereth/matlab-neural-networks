823404468b5a8731b2f7796eb34203dd4d99b30e
Alexander Hoereth <a.hoereth@gmail.com>
2016-01-20 14:11:34 +0100

    Simplify logistic function application.

--- applyNeuralNet.m
+++ applyNeuralNet.m
@@ -7,8 +7,7 @@ function [output, values] = applyNeuralNet(neuralNet, input)
   % Apply neural network to input layer by layer.
   for i = 1:length(neuralNet)
     % Application of logistic function.
-    x = values{i} * neuralNet{i};
-    values{i+1} = 1./(1+exp(-(x)));
+    values{i+1} = 1./(1+exp(-( values{i} * neuralNet{i} )));
   end
 
   % Return output layer values individually.
