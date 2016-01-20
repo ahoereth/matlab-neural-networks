3efe8d6a3272c63a54f107f8b30348d31f84891a
Alexander Hoereth <a.hoereth@gmail.com>
2016-01-20 15:31:31 +0100

    Add documentation.

--- generateNeuralNet.m
+++ generateNeuralNet.m
@@ -1,7 +1,17 @@
 function neuralNet = generateNeuralNet(layers)
+%generateNeuralNet generates a neural network with random weights.
+%
+%  *  `layers` is a vector of doubles, each number specifing the amount of
+%     nodes in a layer of the network.
+%
+%  *  `neuralNet` is a cell array of weight matrices specifing the
+%     translation from one layer of the network to the next.
+
   neuralNet = cell(1, length(layers)-1);
 
   for i = 1:length(layers)-1
+    % Using random weights from -1 to 1 in order to cover the whole area of
+    % the sigmoid/activation function.
     neuralNet{i} = rand(layers(i), layers(i+1)) .* 2 - 1;
   end
 end
