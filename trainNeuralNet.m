f266a061dd0195a5183ebbc89cd46edc7775d585
Alexander Hoereth <a.hoereth@gmail.com>
2016-01-20 15:43:26 +0100

    Fix final total error message. (and whitespace.. MATLAB does not know how to remove trailing whitespace.)

--- trainNeuralNet.m
+++ trainNeuralNet.m
@@ -1,11 +1,11 @@
 function neuralNet = trainNeuralNet(neuralNet, iterations, input, target)
-%trainNeuralNetStatic trains a given neural network of three layers using a 
+%trainNeuralNetStatic trains a given neural network of three layers using a
 % recursive approach for an (theoretically) unlimited amount of layers.
 %
-%`generateNeuralNet` function trains the `neuralNet` `iterations` times 
+%`generateNeuralNet` function trains the `neuralNet` `iterations` times
 % using the mapping from `input` to `targetOutput` cell values.
-%   
-% *  `neuralNet` contains a cell array length 2 of individual weight 
+%
+% *  `neuralNet` contains a cell array length 2 of individual weight
 %    matrixes each describing the transition from one layer to the next.
 % *  `iterations` is an integer greater 0.
 % *  `input` is a cell array (1d) of possible input vectors (`1*n double`)
@@ -17,11 +17,11 @@ function neuralNet = trainNeuralNet(neuralNet, iterations, input, target)
 
   % Should be something small in order to not overshoot the goal.
   LEARNINGRATE = .01;
-  
+
   % Total error value on which to break.
   EPS = 0.0001;
-  
-  
+
+
   % Placeholder. Error will be calculated in every iteration.
   totalError = Inf;
 
@@ -30,36 +30,36 @@ function neuralNet = trainNeuralNet(neuralNet, iterations, input, target)
   for iter = 1:iterations
     sets = size(input, 1);
     totalError = totalError / sets;
-    
+
     % Tell us about the progress every 1000 iterations.
     if (mod(iter, 1000) == 0)
       display([int2str(iter), ': ', num2str(totalError)])
     end
-    
+
     % Stop training when the total error is good enough.
     if (totalError < EPS)
       break;
     end
-    
+
     % Reset total error.
     totalError = 0;
-  
-    % Train network using each input/targetoutput pair. 
+
+    % Train network using each input/targetoutput pair.
     for set = 1:sets
       targetOutput = target(set, :);
 
       [~, values] = applyNeuralNet(neuralNet, input(set, :));
       totalError = totalError + sum(abs((targetOutput - values{end})))/set;
-      
+
       % Initial error on output layer.
       e = (values{end} - targetOutput)';
-      
+
       % Backpropagate through network -- iterate over the weight layer
       % from right to left.
       for i = length(neuralNet):-1:1;
         out = values{i+1}(:); % Current weight layers original output.
         in = values{i}; % Current weight layers original input.
-        
+
         % delta = diag(out .* (1 - out)) * e(:); % Mathematical way.
         delta = (out .* (1 - out)) .* e(:); % MATLAB way.
         e = neuralNet{i} * delta; % Next error for backpropagation.
@@ -67,9 +67,8 @@ function neuralNet = trainNeuralNet(neuralNet, iterations, input, target)
       end
     end
   end
-  
+
   % Tell us about the result.
   display(['Stopped training at ', int2str(iter), ...
-    ' iterations with a total error of ', num2str(totalError)])
+    ' iterations with a total error of ', num2str(totalError/sets)])
 end
-
--- trainNeuralNetStatic.m
+++ trainNeuralNetStatic.m
@@ -1,15 +1,15 @@
-function neuralNet = trainNeuralNetStatic(neuralNet, iterations, input, target)  
+function neuralNet = trainNeuralNetStatic(neuralNet, iterations, input, target)
 %trainNeuralNetStatic trains a given neural network of three layers (input,
 %hidden, output) using a non recursive approach.
 %
-%`generateNeuralNet` function trains the `neuralNet` `iterations` times 
+%`generateNeuralNet` function trains the `neuralNet` `iterations` times
 % using the mapping from `input` to `targetOutput` cell values.
-%   
-% *  `neuralNet` contains a cell array length 2 of individual weight 
+%
+% *  `neuralNet` contains a cell array length 2 of individual weight
 %    matrixes each describing the transition from one layer to the next.
 % *  `iterations` is an integer greater 0.
 % *  `input` is a cell array (1d) of possible input vectors (`1*n double`)
-% *  `target` is a cell array of expected output vectors to those in 
+% *  `target` is a cell array of expected output vectors to those in
 %    `input`.
 %
 % Very verbose code in order to closly match the referenced approach and
@@ -20,33 +20,33 @@ function neuralNet = trainNeuralNetStatic(neuralNet, iterations, input, target)
 
   % Should be something small in order to not overshoot the goal.
   LEARNINGRATE = .01;
-  
+
   % Total error value on which to break.
   EPS = .01;
-  
+
   % Placeholder. Error will be calculated in every iteration.
   totalError = Inf;
-  
+
   % Train the network `iterations` times.
   display('Training...');
   for iter = 1:iterations
-    
+
     % Tell us about the progress every 1000 iterations.
     if (mod(iter, 1000) == 0)
       display([int2str(iter), ': ', num2str(totalError)])
     end
-    
-    % Stop training when the total error is good enough.    
+
+    % Stop training when the total error is good enough.
     if (totalError < EPS)
       break;
     end
-    
+
     % Reset total error.
     totalError = 0;
-  
+
     sets = size(input, 1);
 
-    % Train network using each input/targetoutput pair. 
+    % Train network using each input/targetoutput pair.
     for set = 1:sets
       t = target(set, :); % target output regarding current input
 
@@ -63,19 +63,18 @@ function neuralNet = trainNeuralNetStatic(neuralNet, iterations, input, target)
       e = o2(:) - t(:); % deviation
       delta2 = D2 * e;
       delta1 = D1 * W2 * delta2;
-      
+
       % Correction matrices.
       correctionsW2 = -LEARNINGRATE * delta2 * o1;
       correctionsW1 = -LEARNINGRATE * delta1 * o;
-      
+
       % Weight updates.
       neuralNet{2} = W2 + correctionsW2';
       neuralNet{1} = W1 + correctionsW1';
     end
   end
-  
+
   % Tell us about the result.
   display(['Stopped training at ', int2str(iter), ...
-    ' iterations with a total error of ', num2str(totalError)])
+    ' iterations with a total error of ', num2str(totalError/sets)])
 end
-
