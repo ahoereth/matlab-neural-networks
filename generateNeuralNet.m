3059405edb6eee11132a475fc2c7e01d3eda068a
Alexander Hoereth <a.hoereth@gmail.com>
2016-01-20 14:11:54 +0100

    Make initial weights range from -1 to 1.

--- generateNeuralNet.m
+++ generateNeuralNet.m
@@ -2,6 +2,6 @@ function neuralNet = generateNeuralNet(layers)
   neuralNet = cell(1, length(layers)-1);
 
   for i = 1:length(layers)-1
-    neuralNet{i} = rand(layers(i), layers(i+1)) - .5;
+    neuralNet{i} = rand(layers(i), layers(i+1)) .* 2 - 1;
   end
 end
